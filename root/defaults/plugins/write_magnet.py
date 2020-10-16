6c6548f0c82bfb9177123bd57f4340bb3e0658ea
wiserain <mail275@gmail.com>
2020-10-16 11:39:07 +0900

    Update write_magnet.py
    - check compatibility with libtorrent versions 1.1.5 1.1.13 1.2.5+
    - opt: use_dht
    - opt: http_prox (need test)
    - apply setting pack for libtorrent options
    - scrape by default
    - extra entry fileds

--- root/defaults/plugins/write_magnet.py
+++ root/defaults/plugins/write_magnet.py
@@ -1,5 +1,7 @@
 import os
 import time
+from datetime import datetime
+from urllib.parse import urlparse
 
 from loguru import logger
 
@@ -24,6 +26,8 @@ class ConvertMagnet:
                     "timeout": {"type": "string", "format": "interval"},
                     "force": {"type": "boolean"},
                     "num_try": {"type": "integer"},
+                    "use_dht": {"type": "boolean"},
+                    "http_proxy": {"type": "string", "format": "uri"},
                 },
                 "additionalProperties": False,
             },
@@ -33,90 +37,173 @@ class ConvertMagnet:
     def __init__(self):
         try:
             import requests
-            trackers_url_from = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt'
-            self.trackers = requests.get(trackers_url_from).content.decode('utf8').split('\n\n')[:-1]
+            trackers_from = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt'
+            self.trackers = requests.get(trackers_from).content.decode('utf8').split('\n\n')[:-1]
         except Exception as e:
-            logger.debug('Failed to get tracker list: {}', str(e))
+            logger.debug('Failed to get trackers from {}: {}', trackers_from, str(e))
             self.trackers = []
 
-    def magnet_to_torrent(self, magnet_uri, destination_folder, timeout, num_try):
-        import libtorrent
+    def convert_torrent_info(self, torrent_info):
+        """from libtorrent torrent_info to python dictionary object"""
+        import libtorrent as lt
+
+        return {
+            'name': torrent_info.name(),
+            'num_files': torrent_info.num_files(),
+            'total_size': torrent_info.total_size(),  # in byte
+            'info_hash': str(torrent_info.info_hash()),  # original type: libtorrent.sha1_hash
+            'num_pieces': torrent_info.num_pieces(),
+            'creator': torrent_info.creator() if torrent_info.creator() else 'libtorrent v{}'.format(lt.version),
+            'comment': torrent_info.comment(),
+            'files': [{'path': file.path, 'size': file.size} for file in torrent_info.files()],
+            'magnet_uri': lt.make_magnet_uri(torrent_info),
+        }
+
+    def magnet_to_torrent(self, magnet_uri, destination_folder, timeout, num_try, use_dht, http_proxy):
+        import libtorrent as lt
 
         # parameters
-        params = libtorrent.parse_magnet_uri(magnet_uri)
+        params = lt.parse_magnet_uri(magnet_uri)
 
         # prevent downloading
         # https://stackoverflow.com/q/45680113
-        params.flags |= libtorrent.add_torrent_params_flags_t.flag_upload_mode
+        if isinstance(params, dict):
+            params['flags'] |= lt.add_torrent_params_flags_t.flag_upload_mode
+        else:
+            params.flags |= lt.add_torrent_params_flags_t.flag_upload_mode
         
-        lt_version = [int(v) for v in libtorrent.version.split('.')]
+        lt_version = [int(v) for v in lt.version.split('.')]
         if [0, 16, 13, 0] < lt_version < [1, 1, 3, 0]:
             # for some reason the info_hash needs to be bytes but it's a struct called sha1_hash
-            params.info_hash = params.info_hash.to_bytes()
+            if isinstance(params, dict):
+                params['info_hash'] = params['info_hash'].to_bytes()
+            else:
+                params.info_hash = params.info_hash.to_bytes()
 
-        # add_trackers
-        if len(params.trackers) == 0:
-            try:
-                import random
-                params.trackers = random.sample(self.trackers, 5)
-            except Exception as e:
-                logger.debug('Failed to add trackers: {}', str(e))
-        
-        # session
-        session = libtorrent.session()
+        # add_trackers - currently always append
+        try:
+            if isinstance(params, dict):
+                params['trackers'] += self.trackers
+            else:
+                params.trackers += self.trackers
+        except Exception as e:
+            logger.debug('Failed to add trackers: {}', str(e))
 
-        session.listen_on(6881, 6891)
-        
-        session.add_extension('ut_metadata')
-        session.add_extension('ut_pex')
-        session.add_extension('metadata_transfer')
+        # session from setting pack
+        settings = {
+            # basics
+            # 'user_agent': 'libtorrent/' + lt.__version__,
+            'listen_interfaces': '0.0.0.0:6881',
+            # dht
+            'enable_dht': use_dht,
+            'use_dht_as_fallback': True,
+            'dht_bootstrap_nodes': 'router.bittorrent.com:6881,dht.transmissionbt.com:6881,router.utorrent.com:6881,127.0.0.1:6881',
+            'enable_lsd': False,
+            'enable_upnp': True,
+            'enable_natpmp': True,
+            'announce_to_all_tiers': True,
+            'announce_to_all_trackers': True,
+            'aio_threads': 4*2,
+            'checking_mem_usage': 1024*2,
+        }
+        if http_proxy:
+            # TODO: TEST http_proxy
+            proxy_url = urlparse(http_proxy)
+            logger.debug(proxy_url)
+            settings.update({
+                'proxy_username': proxy_url.username,
+                'proxy_password': proxy_url.password,
+                'proxy_hostname': proxy_url.hostname,
+                'proxy_port': proxy_url.port,
+                'proxy_type': lt.proxy_type_t.http_pw if proxy_url.username and proxy_url.password else lt.proxy_type_t.http,
+                'force_proxy': True,
+                'anonymous_mode': True,
+            })
+        session = lt.session(settings)
 
-        session.add_dht_router('router.utorrent.com', 6881)
-        session.add_dht_router('router.bittorrent.com', 6881)
-        session.add_dht_router("dht.transmissionbt.com", 6881)
-        session.add_dht_router('127.0.0.1', 6881)
-        session.start_dht()
+        # session.add_extension('ut_metadata')
+        # session.add_extension('ut_pex')
+        # session.add_extension('metadata_transfer')
 
         # handle
         handle = session.add_torrent(params)
-        handle.force_dht_announce()
+
+        if use_dht:
+            handle.force_dht_announce()
+
         logger.debug('Acquiring torrent metadata for magnet {}', magnet_uri)
         
-        for tryid in range(max(num_try,1)):
+        max_try = max(num_try, 1)
+        for tryid in range(max_try):
             timeout_value = timeout
+            logger.debug('Trying to get metadata ... {}/{}'.format(tryid+1, max_try))
             while not handle.has_metadata():
                 time.sleep(0.1)
                 timeout_value -= 0.1
                 if timeout_value <= 0:
-                    logger.debug('Failed to get metadata on trial: {}/{}'.format(tryid+1, num_try))
                     break
 
             if handle.has_metadata():
-                logger.debug('Metadata acquired after {} seconds on trial {}'.format(timeout - timeout_value, tryid+1))
+                lt_info = handle.get_torrent_info()
+                logger.debug('Metadata acquired after {}*{}+{:.1f} seconds', tryid, timeout, timeout - timeout_value)
                 break
             else:
-                if tryid+1 == max(num_try,1):
+                if tryid+1 == max_try:
                     session.remove_torrent(handle, True)
                     raise plugin.PluginError(
-                        'Timed out after {}x{} seconds trying to magnetize'.format(timeout, num_try)
+                        'Timed out after {}*{} seconds'.format(max_try, timeout)
                     )
     
-        torrent_info = handle.get_torrent_info()
-        torrent_file = libtorrent.create_torrent(torrent_info)
+        # create torrent object
+        torrent = lt.create_torrent(lt_info)
+        torrent.set_creator('libtorrent v{}'.format(lt.version))    # signature
+        torrent_dict = torrent.generate()
+
+        torrent_info = self.convert_torrent_info(lt_info)
+        torrent_info.update({
+            'trackers': params['trackers'] if isinstance(params, dict) else params.trackers,
+            'creation_date': datetime.fromtimestamp(torrent_dict[b'creation date']).isoformat(),
+        })
+        
+        # start scraping
+        timeout_value = timeout
+        logger.debug('Trying to get peerinfo ... ')
+        while handle.status(0).num_complete < 0:
+            time.sleep(0.1)
+            timeout_value -= 0.1
+            if timeout_value <= 0:
+                break
+
+        if handle.status(0).num_complete >= 0:
+            torrent_status = handle.status(0)
+            logger.debug('Peerinfo acquired after {:.1f} seconds', timeout - timeout_value)
+            
+            torrent_info.update({
+                'seeders': torrent_status.num_complete,
+                'peers': torrent_status.num_incomplete,
+            })
+        else:
+            raise plugin.PluginError('Timed out after {} seconds'.format(timeout))
+
+        session.remove_torrent(handle, True)
+
         torrent_path = pathscrub(
-            os.path.join(destination_folder, torrent_info.name() + ".torrent")
+            os.path.join(destination_folder, lt_info.name() + ".torrent")
         )
         with open(torrent_path, "wb") as f:
-            f.write(libtorrent.bencode(torrent_file.generate()))
+            f.write(lt.bencode(torrent_dict))
         logger.debug('Torrent file wrote to {}', torrent_path)
-        return torrent_path
+
+        return torrent_path, torrent_info
 
     def prepare_config(self, config):
         if not isinstance(config, dict):
             config = {}
-        config.setdefault('timeout', '10 seconds')
+        config.setdefault('timeout', '15 seconds')
         config.setdefault('force', False)
-        config.setdefault('num_retry', 3)
+        config.setdefault('num_try', 3)
+        config.setdefault('use_dht', True)
+        config.setdefault('http_proxy', '')
         return config
 
     @plugin.priority(plugin.PRIORITY_FIRST)
@@ -148,7 +235,9 @@ class ConvertMagnet:
                 entry.setdefault('urls', [entry['url']])
                 try:
                     logger.info('Converting entry {} magnet URI to a torrent file', entry['title'])
-                    torrent_file = self.magnet_to_torrent(entry['url'], converted_path, timeout, config['num_try'])
+                    torrent_file, torrent_info = self.magnet_to_torrent(
+                        entry['url'], converted_path, timeout, config['num_try'], config['use_dht'], config['http_proxy']
+                        )
                 except (plugin.PluginError, TypeError) as e:
                     logger.error(
                         'Unable to convert Magnet URI for entry {}: {}', entry['title'], e
@@ -164,7 +253,14 @@ class ConvertMagnet:
                 # make sure it's first in the list because of how download plugin works
                 entry['urls'].insert(0, 'file://{}'.format(torrent_file))
 
+                # TODO: could be populate extra fields from torrent_info
+                entry['content_size'] = torrent_info['total_size']
+                entry['seeders'] = torrent_info['seeders']                
+                entry['leechers'] = torrent_info['peers']
+
 
 @event('plugin.register')
 def register_plugin():
     plugin.register(ConvertMagnet, 'write_magnet', api_ver=2)
+
+# confirmed working with libtorrent version 1.1.5 1.1.13 1.2.5+
